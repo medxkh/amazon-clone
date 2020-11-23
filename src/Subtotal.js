cc51c64ddfa136a937747b6d6085ed56a6128788
medxkh <medkh6699@gmail.com>
2020-11-23 14:27:58 +0100

    fix subtotal / add more styles

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -11,17 +11,18 @@ function Subtotal() {
             <p>
               Subtotal (0 items): <strong>0</strong>
             </p>
-            <small className="subtottal_gift">
-              <input type="checkbox" /> order to
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
             </small>
           </>
         )}
         decimalScale={2}
         value={0}
-        diplayType={"text"}
+        displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
       />
+      <button> Proceed to Checkout </button>
     </div>
   );
 }
