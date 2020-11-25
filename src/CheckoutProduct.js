8229d8d5bce8addd8001471685079249357c0fc9
medxkh <medkh6699@gmail.com>
2020-11-25 14:19:11 +0100

    add remove from basket function

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ id, img, title, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  const removeFromBasket = () => {
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={img} />
@@ -18,7 +26,7 @@ function CheckoutProduct({ id, img, title, price, rating }) {
               <p>🌟</p>
             ))}
         </div>
-        <button>Remove from basket</button>
+        <button onClick={removeFromBasket}>Remove from basket</button>
       </div>
     </div>
   );
